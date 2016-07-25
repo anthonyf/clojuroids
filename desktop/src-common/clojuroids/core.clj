636d49432c35bc7988279af611a8ce511355aa08
Anthony Fairchild <fairchild.anthony@gmail.com>
2016-07-25 19:32:59 -0400

    part 2 - setting up camera

--- desktop/src-common/clojuroids/core.clj
+++ desktop/src-common/clojuroids/core.clj
@@ -1,19 +1,26 @@
 (ns clojuroids.core
-  (:import [com.badlogic.gdx ApplicationListener
-            Gdx]
-           [com.badlogic.gdx.graphics GL30]))
+  (:import [com.badlogic.gdx ApplicationListener Gdx]
+           [com.badlogic.gdx.graphics GL30 OrthographicCamera]))
 
 (def app-listener
-  (reify
-    ApplicationListener
+  (let [camera-ref (atom nil)
+        screen-size-ref (atom [])]
+    (reify
+      ApplicationListener
 
-    (create [this])
+      (create [this]
+        (reset! screen-size-ref [(.getWidth Gdx/graphics)
+                                 (.getHeight Gdx/graphics)])
+        (let [[width height] @screen-size-ref]
+          (reset! camera-ref (OrthographicCamera. width height))
+          (.translate @camera-ref (/ width 2) (/ height 2))
+          (.update @camera-ref)))
 
-    (render [this]
-      (.glClearColor Gdx/gl 0 0 0 1)
-      (.glClear Gdx/gl GL30/GL_COLOR_BUFFER_BIT))
+      (render [this]
+        (.glClearColor Gdx/gl 0 0 0 1)
+        (.glClear Gdx/gl GL30/GL_COLOR_BUFFER_BIT))
 
-    (resize [this width height])
-    (pause [this])
-    (resume [this])
-    (dispose [this])))
+      (resize [this width height])
+      (pause [this])
+      (resume [this])
+      (dispose [this]))))
