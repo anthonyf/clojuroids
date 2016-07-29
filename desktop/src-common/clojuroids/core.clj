cdca08a42ca37f7fae35c6e4852da043cff15a76
Anthony Fairchild <fairchild.anthony@gmail.com>
2016-07-29 12:19:34 -0400

    part 3 - keyboard input

--- desktop/src-common/clojuroids/core.clj
+++ desktop/src-common/clojuroids/core.clj
@@ -1,10 +1,12 @@
 (ns clojuroids.core
+  (:require [clojuroids.key-state :refer :all])
   (:import [com.badlogic.gdx ApplicationListener Gdx]
            [com.badlogic.gdx.graphics GL30 OrthographicCamera]))
 
 (def app-listener
   (let [camera-ref (atom nil)
-        screen-size-ref (atom [])]
+        screen-size-ref (atom [])
+        key-state-ref (atom {})]
     (reify
       ApplicationListener
 
@@ -14,11 +16,15 @@
         (let [[width height] @screen-size-ref]
           (reset! camera-ref (OrthographicCamera. width height))
           (.translate @camera-ref (/ width 2) (/ height 2))
-          (.update @camera-ref)))
+          (.update @camera-ref))
+
+        (.setInputProcessor Gdx/input (make-input-processor key-state-ref)))
 
       (render [this]
         (.glClearColor Gdx/gl 0 0 0 1)
-        (.glClear Gdx/gl GL30/GL_COLOR_BUFFER_BIT))
+        (.glClear Gdx/gl GL30/GL_COLOR_BUFFER_BIT)
+
+        (swap! key-state-ref update-key-state))
 
       (resize [this width height])
       (pause [this])
