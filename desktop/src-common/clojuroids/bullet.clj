afbaf52f9717de08c7af5bf329cb122ceda06752
Anthony Fairchild <fairchild.anthony@gmail.com>
2016-07-30 16:13:34 -0400

    part 6.2 - bullets

---
+++ desktop/src-common/clojuroids/bullet.clj
@@ -0,0 +1,69 @@
+(ns clojuroids.bullet
+  (:require [clojuroids.space-object :as so])
+  (:import [com.badlogic.gdx.math MathUtils]))
+
+(defrecord Bullet
+  [space-object life-time life-timer remove?])
+
+(defn make-bullet
+  [[x y] radians]
+  (let [speed 350]
+    (map->Bullet {:space-object (so/map->SpaceObject {:pos     [x y]
+                                                      :radians radians
+                                                      :dpos    [(* (MathUtils/cos radians) speed)
+                                                                (* (MathUtils/sin radians) speed)]
+                                                      :size    [2 2]})
+                  :remove?      false
+                  :life-timer   0
+                  :life-time    1})))
+
+(defn make-bullet-shape
+  [bullet]
+  (let [{:keys [space-object]} bullet
+        {:keys [pos radians]} space-object
+        [x y] pos]
+    [[(+ x 1)
+      (+ y 1)]
+     [(+ x 1)
+      (- y 1)]
+     [(- x 1)
+      (- y 1)]]))
+
+(defn update-bullet
+  [bullet screen-size delta-time]
+  (as-> bullet b
+        (update b :space-object #(-> %
+                                     (so/update-space-object! screen-size delta-time)
+                                     (assoc :shape (make-bullet-shape b))))
+        (update b :life-timer #(+ % delta-time))
+        (assoc b :remove? (let [{:keys [life-timer life-time]} b]
+                            (> life-timer life-time)))))
+
+(defn update-bullets
+  [bullets screen-size delta-time]
+  (->> bullets
+       (map #(update-bullet % screen-size delta-time))
+       (remove :remove?)
+       (into #{})))
+
+(def bullet-color [1 1 1 1])
+
+(defn draw-bullet
+  [bullet shape-renderer]
+  (let [{:keys [space-object]} bullet]
+    (so/draw-space-object space-object shape-renderer bullet-color)))
+
+(defn draw-bullets
+  [bullets shape-renderer]
+  (doseq [bullet bullets]
+    (draw-bullet bullet shape-renderer)))
+
+(def ^:const max-bullets 4)
+
+(defn shoot
+  [player bullets]
+  (let [{:keys [space-object]} player
+        {:keys [pos radians]} space-object]
+    (when (< (count bullets) max-bullets)
+      (conj bullets
+            (make-bullet pos radians)))))
--- desktop/src-common/clojuroids/play_state.clj
+++ desktop/src-common/clojuroids/play_state.clj
@@ -1,12 +1,14 @@
 (ns clojuroids.play-state
   (:require [clojuroids.game-state-manager :as gsm]
             [clojuroids.player :as p]
+            [clojuroids.bullet :as b]
             [clojuroids.key-state :as ks])
   (:import [com.badlogic.gdx.graphics.glutils ShapeRenderer]
            (com.badlogic.gdx Input$Keys))
   (:gen-class))
 
-(defrecord PlayState [screen-size-ref key-state-ref shape-renderer player]
+(defrecord PlayState [screen-size-ref key-state-ref shape-renderer player
+                      bullets]
   gsm/game-state
   (init [this]
     (merge this
@@ -14,16 +16,24 @@
             :player (p/make-player @screen-size-ref)}))
 
   (update! [this screen-size delta-time]
-    (update this :player #(p/update-player! % screen-size delta-time)))
+    (-> this
+        (update :player #(p/update-player! % screen-size delta-time))
+        (update :bullets #(b/update-bullets % screen-size delta-time))))
 
   (draw [this]
-    (p/draw-player player shape-renderer))
+    (p/draw-player player shape-renderer)
+    (b/draw-bullets bullets shape-renderer))
 
   (handle-input [this]
-    (assoc this :player (-> player
-                            (assoc :left? (ks/key-down? @key-state-ref Input$Keys/LEFT))
-                            (assoc :right? (ks/key-down? @key-state-ref Input$Keys/RIGHT))
-                            (assoc :up? (ks/key-down? @key-state-ref Input$Keys/UP)))))
+    (as-> this state
+      (assoc state :player (-> player
+                               (assoc :left? (ks/key-down? @key-state-ref Input$Keys/LEFT))
+                               (assoc :right? (ks/key-down? @key-state-ref Input$Keys/RIGHT))
+                               (assoc :up? (ks/key-down? @key-state-ref Input$Keys/UP))))
+      (assoc state :bullets (if (ks/key-pressed? @key-state-ref Input$Keys/SPACE)
+                              (let [{:keys [player bullets]} state]
+                                (b/shoot player bullets))
+                              bullets))))
   (dispose [this]))
 
 (defn make-play-state
