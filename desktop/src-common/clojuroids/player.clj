5ed2171360a78c6ffcb1852f80b0a480590fa3bf
Anthony Fairchild <fairchild.anthony@gmail.com>
2016-08-04 12:43:30 -0400

    part 9.1 - ship explosion

--- desktop/src-common/clojuroids/player.clj
+++ desktop/src-common/clojuroids/player.clj
@@ -4,7 +4,10 @@
 
 (defrecord Player [space-object
                    left? right? up? max-speed acceleration deceleration
-                   accelerating-timer flame])
+                   accelerating-timer flame
+                   hit? dead?
+                   hit-lines hit-lines-vector
+                   hit-timer hit-time])
 
 (defn- make-ship-shape
   [player]
@@ -75,41 +78,62 @@
 
 (defn update-player!
   [player screen-size delta-time]
-  (let [{:keys [left? right? up? accelerating-timer]} player]
-    (as-> player p
-      ;; turning
-      (cond left? (turn-left p delta-time)
-            right? (turn-right p delta-time)
-            :else p)
-      ;; acceleration
-      (if up?
-        (-> p
-            (accelerate delta-time)
-            (assoc :accelerating-timer
-                   (as-> accelerating-timer at
-                     (+ at delta-time)
-                     (if (> at 0.1)
-                       0
-                       at))))
-        p)
-      ;; deceleration
-      (decelerate p  delta-time)
-      ;; update shapes
-      (assoc-in p [:space-object :shape] (make-ship-shape p))
-      (assoc p :flame (if up?
-                        (make-flame-shape p)
-                        []))
-      ;; update space-object
-      (assoc p :space-object (update-space-object! (:space-object p) screen-size delta-time)))))
+  (let [{:keys [left? right? up? accelerating-timer
+                hit? hit-timer hit-time hit-lines
+                hit-lines-vector]} player]
+    (if hit?
+      ;; update dead player
+      (-> player
+          (merge (let [hit-timer (+ hit-timer delta-time)]
+                   (if (> hit-timer hit-time)
+                     {:hit-timer 0
+                      :dead? true}
+                     {:hit-timer hit-timer})))
+          (update :hit-lines (fn [hit-lines]
+                               (map (fn [[[x1 y1] [x2 y2]] [vx vy]]
+                                      [[(+ x1 (* vx 10 delta-time))
+                                        (+ y1 (* vy 10 delta-time))]
+                                       [(+ x2 (* vx 10 delta-time))
+                                        (+ y2 (* vy 10 delta-time))]])
+                                    hit-lines
+                                    hit-lines-vector))))
+      ;; update alive player
+      (as-> player p
+        ;; turning
+        (cond left? (turn-left p delta-time)
+              right? (turn-right p delta-time)
+              :else p)
+        ;; acceleration
+        (if up?
+          (-> p
+              (accelerate delta-time)
+              (assoc :accelerating-timer
+                     (as-> accelerating-timer at
+                       (+ at delta-time)
+                       (if (> at 0.1)
+                         0
+                         at))))
+          p)
+        ;; deceleration
+        (decelerate p  delta-time)
+        ;; update shapes
+        (assoc-in p [:space-object :shape] (make-ship-shape p))
+        (assoc p :flame (if up?
+                          (make-flame-shape p)
+                          []))
+        ;; update space-object
+        (assoc p :space-object (update-space-object! (:space-object p) screen-size delta-time))))))
 
 (def player-color [1 1 1 1])
 
 (defn draw-player
   [player shape-renderer]
-  (let [{:keys [space-object up? flame]} player]
-    (draw-space-object space-object shape-renderer player-color)
-    (when up?
-      (draw-shape shape-renderer flame player-color))))
+  (let [{:keys [space-object up? flame hit? hit-lines]} player]
+    (if hit?
+      (draw-lines shape-renderer hit-lines player-color)
+      (do (draw-space-object space-object shape-renderer player-color)
+          (when up?
+            (draw-shape shape-renderer flame player-color))))))
 
 (defn make-player
   [[screen-width screen-height]]
@@ -124,9 +148,29 @@
                   :deceleration 10
                   :max-speed 300
                   :flame []
-                  :accelerating-timer 0})))
+                  :accelerating-timer 0
+                  :hit? false
+                  :hit-timer 0
+                  :hit-time 2})))
 
 (defn player-hit
   [player]
-  (println "player-hit!")
-  player)
+  (let [{hit? :hit?
+         {:keys [shape radians]} :space-object} player]
+    (if-not hit?
+      (as-> player player
+        (merge player {:hit? true
+                       :left? false
+                       :right? false
+                       :up? false
+                       :hit-lines (vert-lines-seq shape)
+                       :hit-lines-vector [[(MathUtils/cos (+ radians 1.5))
+                                           (MathUtils/sin (+ radians 1.5))]
+                                          [(MathUtils/cos (- radians 1.5))
+                                           (MathUtils/sin (- radians 1.5))]
+                                          [(MathUtils/cos (- radians 2.8))
+                                           (MathUtils/sin (- radians 2.8))]
+                                          [(MathUtils/cos (+ radians 2.8))
+                                           (MathUtils/sin (+ radians 2.8))]]})
+        (update player :space-object (fn [so] (assoc so :dpos [0 0]))))
+      player)))
