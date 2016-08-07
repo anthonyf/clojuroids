c9a13fcaf847de762fdce3a85ae35edda6995a6f
Anthony Fairchild <fairchild.anthony@gmail.com>
2016-08-07 13:07:33 -0400

    part 10 - add font dependencies

--- desktop/project.clj
+++ desktop/project.clj
@@ -1,15 +1,16 @@
 (defproject clojuroids "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  
+
   :dependencies [[com.badlogicgames.gdx/gdx "1.9.3"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "1.9.3"]
                  [com.badlogicgames.gdx/gdx-platform "1.9.3"
                   :classifier "natives-desktop"]
                  [com.badlogicgames.gdx/gdx-freetype "1.9.3"]
+                 [com.badlogicgames.gdx/gdx-freetype-platform "1.9.3"
+                  :classifier "natives-desktop"]
                  [org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.trace "0.7.9"]
-                 ]
-  
+                 [org.clojure/tools.trace "0.7.9"]]
+
   :source-paths ["src" "src-common"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [clojuroids.core.desktop-launcher]
