030ffb05dc36cb0da880db53fc86c4f6e363d7e1
Anthony Fairchild <fairchild.anthony@gmail.com>
2016-08-25 19:45:59 -0400

    back out restartable changes

--- src/clojuroids/game_state_manager.clj
+++ src/clojuroids/game_state_manager.clj
@@ -7,7 +7,6 @@
 (def game-states (atom {}))
 (def current-state (atom nil))
 (def next-state (atom nil))
-(def restartable? false)
 
 (defprotocol GameState
   "protocol for game states"
@@ -24,27 +23,14 @@
   [key & args]
   (reset! next-state {:key key :args args}))
 
-(defrecord ErrorState []
-  GameState
-  (init [this] init)
-  (draw [this])
-  (update! [this delta-time] this)
-  (dispose [this]))
-
 (defn update-game-state!
   []
-  (try
-    (when-not (nil? @next-state)
-      (when-not (nil? @current-state)
-        (dispose @current-state))
-      (let [{:keys [key args]} @next-state]
-        (reset! current-state (init (apply (key @game-states)
-                                           args))))
-      (reset! next-state nil))
+  (when-not (nil? @next-state)
+    (when-not (nil? @current-state)
+      (dispose @current-state))
+    (let [{:keys [key args]} @next-state]
+      (reset! current-state (init (apply (key @game-states)
+                                         args))))
+    (reset! next-state nil))
     (swap! current-state update! (.getDeltaTime Gdx/graphics))
-    (draw @current-state)
-    (catch Exception e
-      (if restartable?
-        (do (reset! current-state (ErrorState.))
-            (st/print-cause-trace e))
-        (throw e)))))
+    (draw @current-state))
